d99863ac86e98e8a958b24912618251851fbd931
shupon0495 <shun2nakano057@gmail.com>
2026-09-09 10:23:27 +0900

    add: job実行の初めにsingularityのsifファイル構築の内容を追加

--- slurm/submit_pipeline.sh
+++ slurm/submit_pipeline.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
+set -e
 
-JOB1=$(sbatch --parsable preprocess.sh)
-JOB2=$(sbatch --parsable --dependency=afterok:$JOB1 train.sh)
-JOB3=$(sbatch --parsable --dependency=afterok:$JOB2 test.sh)
+PROJECT=/home/users/$USER/vla_simulation_source
+DEF=$PROJECT/singularity/ubuntu24.04.def
+SIF=$PROJECT/singularity/ubuntu24.04.sif
+LOG=$PROJECT/log
+
+# sifがないかdefのほうがsifより新しいときにdefを作成する
+# もしloginノード内でbuildをするのが禁止されていたらjobに変更するようにする
+if [ ! -f "$SIF" ] || [ "$DEF" -nt "$SIF" ]; then
+    echo "Building Singularity image..."
+    singularity build --force "$SIF" "$DEF"
+else
+    echo "Singularity image is up to date."
+fi
+
+JOB1=$(sbatch --parsable --export=ALL, PROJECT=$PROJECT, LOG=$LOG $PROJECT/slurm/preprocess.sh)
+JOB2=$(sbatch --parsable --export=ALL, PROJECT=$PROJECT, LOG=$LOG --dependency=afterok:$JOB1 $PROJECT/slurm/train.sh)
+JOB3=$(sbatch --parsable --export=ALL, PROJECT=$PROJECT, LOG=$LOG --dependency=afterok:$JOB2 $PROJECT/slurm/test.sh)
 
 echo "preprocess: $JOB1"
 echo "train:      $JOB2"
