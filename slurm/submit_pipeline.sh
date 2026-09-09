f5296b7394a55e501e09fdb381629134adac06fd
shupon0495 <shun2nakano057@gmail.com>
2026-09-09 16:51:40 +0900

    fix: ログファイルの名前を修正

--- slurm/submit_pipeline.sh
+++ slurm/submit_pipeline.sh
@@ -8,10 +8,12 @@ source $PROJECT/slurm/config.sh
 # もしloginノード内でbuildをするのが禁止されていたらjobに変更するようにする
 if [ ! -f "$SIF" ] || [ "$DEF" -nt "$SIF" ]; then
     echo "Building Singularity image..."
-    $(sbatch --parsable --export=ALL,PROJECT=$PROJECT,LOG=$LOG \
-        --output=$LOG/slurm-%j.out \
-        --error=$LOG/slurm-%j.err \
-        $PROJECT/slurm/build.sh
+    $(sbatch --parsable \
+        --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
+        --partition="$PPC_PARTITION" \
+        --output="$LOG/slurm-%j.out" \
+        --error="$LOG/slurm-%j.err" \
+        "$PROJECT/slurm/build.sh"
     )
 else
     echo "Singularity image is up to date."
@@ -20,16 +22,16 @@ fi
 JOB1=$(sbatch --parsable \
     --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
     --partition="$PPC_PARTITION" \
-    --output="$LOG/slurm-%j.out" \
-    --error="$LOG/slurm-%j.err" \
+    --output="$LOG/preprocess-%j.out" \
+    --error="$LOG/preprocess-%j.err" \
     "$PROJECT/slurm/preprocess.sh"
 )
 
 JOB2=$(sbatch --parsable \
     --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
     --partition="$TRAIN_PARTITION" \
-    --output="$LOG/slurm-%j.out" \
-    --error="$LOG/slurm-%j.err" \
+    --output="$LOG/train-%j.out" \
+    --error="$LOG/train-%j.err" \
     --dependency="afterok:$JOB1" \
     "$PROJECT/slurm/train.sh"
 )
@@ -37,8 +39,8 @@ JOB2=$(sbatch --parsable \
 JOB3=$(sbatch --parsable \
     --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
     --partition="$TEST_PARTITION" \
-    --output="$LOG/slurm-%j.out" \
-    --error="$LOG/slurm-%j.err" \
+    --output="$LOG/test-%j.out" \
+    --error="$LOG/test-%j.err" \
     --dependency="afterok:$JOB2" \
     "$PROJECT/slurm/test.sh"
 )
