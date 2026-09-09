e395cc3f8f8345b9031ee46427a1e10870b79e87
shupon0495 <shun2nakano057@gmail.com>
2026-09-09 16:32:28 +0900

    fix: sbatchのオプションの修正

--- slurm/submit_pipeline.sh
+++ slurm/submit_pipeline.sh
@@ -16,17 +16,31 @@ if [ ! -f "$SIF" ] || [ "$DEF" -nt "$SIF" ]; then
 else
     echo "Singularity image is up to date."
 fi
-JOB1=$(sbatch --parsable --export=ALL,PROJECT=$PROJECT,LOG=$LOG \
-    --partition=$PPC_PARTITION --output=$LOG/slurm-%j.out \
-    --error=$LOG/slurm-%j.err $PROJECT/slurm/preprocess.sh
+
+JOB1=$(sbatch --parsable \
+    --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
+    --partition="$PPC_PARTITION" \
+    --output="$LOG/slurm-%j.out" \
+    --error="$LOG/slurm-%j.err" \
+    "$PROJECT/slurm/preprocess.sh"
 )
-JOB2=$(sbatch --parsable --export=ALL,PROJECT=$PROJECT, LOG=$LOG \
-    --partition=$TRAIN_PARTITION --output=$LOG/slurm-%j.out \
-    --error=$LOG/slurm-%j.err --dependency=afterok:$JOB1 $PROJECT/slurm/train.sh
+
+JOB2=$(sbatch --parsable \
+    --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
+    --partition="$TRAIN_PARTITION" \
+    --output="$LOG/slurm-%j.out" \
+    --error="$LOG/slurm-%j.err" \
+    --dependency="afterok:$JOB1" \
+    "$PROJECT/slurm/train.sh"
 )
-JOB3=$(sbatch --parsable --export=ALL,PROJECT=$PROJECT, LOG=$LOG \
-    --partition=$TEST_PARTITION --output=$LOG/slurm-%j.out \
-    --error=$LOG/slurm-%j.err --dependency=afterok:$JOB2 $PROJECT/slurm/test.sh
+
+JOB3=$(sbatch --parsable \
+    --export="ALL,PROJECT=$PROJECT,LOG=$LOG" \
+    --partition="$TEST_PARTITION" \
+    --output="$LOG/slurm-%j.out" \
+    --error="$LOG/slurm-%j.err" \
+    --dependency="afterok:$JOB2" \
+    "$PROJECT/slurm/test.sh"
 )
 
 echo "preprocess: $JOB1"
